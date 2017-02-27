639e2b63b5cd9f43d2f0deddf56bc96d9379c42f
Resheil Agarwal <resheil@resheil.com>
2017-02-28 02:08:37 +0530

    Simple Chat like Server-Client Talk
    Add the functionality so client can send multiple messages. Next is to
    solve the bug for multiple port. Every new message send from the client
    side needs a new port number in this update. Next update will try to
    optimize it.

--- sockets/client.php
+++ sockets/client.php
@@ -1,17 +1,30 @@
 <?php 
+
 $port_file = "portfile.txt";
 $fp = fopen($port_file, 'r'); 
 $port = fgets($fp, 1024); 
 fclose($fp); 
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP); 
-socket_connect($sock, '127.0.0.1', $port); 
-    
-	$st = "Hello from the other side";
+
+	socket_connect($sock, '127.0.0.1', $port); 	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") 
+	{
+    $st=$_POST['param'];
+	}
+	else{
+	$st = "Handshake";
+	}
     $sent = socket_write($sock, $st, strlen($st));
 	if($sent == False){   echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";}else{echo "Done";}
     
     $input = socket_read($sock, 1024) or die("Error");
-    echo $input; 	
-	 
-socket_close($sock); 
+    echo $input; 
+
 ?>
+
+<html>
+<form method="post">
+<input name='param' type='text'>
+<input type='submit' value='GO'>
+</form>
+</html
