70427abd76c28cdc1219e07350b13635ffc074fc
Resheil Agarwal <resheil@resheil.com>
2017-02-27 05:58:04 +0530

    Major Update - Fixed Basic (Read - Write Operation Bothways) and multiple client connection
    The script is completely changed after exploring other available options
    to work on scokets. The whole set of codes is changed and now multiple
    clients can connect to the same socket and message can be send and
    recieved from both ends.

--- sockets/client.php
+++ sockets/client.php
@@ -1,21 +1,17 @@
-<?php
-
-$host    = "127.0.0.1";
-$port    = 25003;
-$message = "Hello Server";
-echo "Message To server :".$message;
-// create socket
-$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-// connect to server
-$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
-// send string to server
-socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
-// get server response
-$result = socket_read ($socket, 1024) or die("Could not read server response\n");
-echo "<br>Reply From Server  :".$result;
-// close socket
-socket_close($socket);
-
-
+<?php 
+$port_file = "portfile.txt";
+$fp = fopen($port_file, 'r'); 
+$port = fgets($fp, 1024); 
+fclose($fp); 
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP); 
+socket_connect($sock, '127.0.0.1', $port); 
+    
+	$st = "Hello from the other side";
+    $sent = socket_write($sock, $st, strlen($st));
+	if($sent == False){   echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";}else{echo "Done";}
+    
+    $input = socket_read($sock, 1024) or die("Error");
+    echo $input; 	
+	 
+socket_close($sock); 
 ?>
-
--- sockets/server.php
+++ sockets/server.php
@@ -1,30 +1,23 @@
-<?php
-
-// set some variables
-$host = "127.0.0.1";
-$port = 25003;
-// don't timeout!
-set_time_limit(0);
-// create socket
-$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-// bind socket to port
-$result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
-// start listening for connections
-$result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
-
-// accept incoming connections
-// spawn another socket to handle communication
-$spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
-// read client input
-$input = socket_read($spawn, 1024) or die("Could not read input\n");
-// clean up input string
-$input = trim($input);
-echo "Client Message : ".$input;
-// reverse client input and send back
-$output = strrev($input) . "\n";
-$output = "<br>Resheil Dominating the world</br>";
-socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
-// close sockets
-socket_close($spawn);
-socket_close($socket);
-?>
+<?php 
+$port_file = "portfile.txt";
+$sock = socket_create_listen(0); 
+socket_getsockname($sock, $addr, $port); 
+print "Server Listening on $addr:$port\n"; 
+$fp = fopen($port_file, 'w'); 
+fwrite($fp, $port); 
+fclose($fp); 
+$message = "Hello, This is server !!";
+while($c = socket_accept($sock)) { 
+   /* do something useful */ 
+   // read client input
+   $input = socket_read($c, 1024) or die("Error");
+   echo $input; 
+   
+   //$message = fgets(STDIN);
+   socket_write($c, fgets(STDIN), strlen ($message)) or die("Error");
+   
+   socket_getpeername($c, $raddr, $rport); 
+   print "Received Connection from $raddr:$rport\n"; 
+}    
+socket_close($sock); 
+?> 
